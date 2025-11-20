b449f73b9ae4116a14fe4b9e7bfa7d04b6484267
LOUIS Romain <romain.louis77@gmail.com>
2025-11-20 14:43:47 +0100

    Mise a jour date aquariumdialog

--- front_end/web/src/app/app.config.ts
+++ front_end/web/src/app/app.config.ts
@@ -10,6 +10,10 @@ import {
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AuthInterceptor } from './core/auth.interceptor';
 
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
@@ -19,5 +23,11 @@ export const appConfig: ApplicationConfig = {
     ),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     provideAnimations(),
+
+    // 👉 Ajout indispensable pour le datepicker
+    { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' },
+    provideNativeDateAdapter(),
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
 };
