6e0ffd97b48319b0fc9eeba168d9f3b029ee52e3
LOUIS Romain <romain.louis77@gmail.com>
2025-12-31 18:53:18 +0100

    email

--- back_end/src/mail/mail.module.ts
+++ back_end/src/mail/mail.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MailService } from './mail.service';
 
+class NoopMailService {
+  async sendVerifyEmail() { return; }
+  async sendResetPassword() { return; }
+}
+
 @Module({
   imports: [ConfigModule],
-  providers: [MailService],
+  providers: [
+    {
+      provide: MailService,
+      useClass: process.env.NODE_ENV === 'test' ? NoopMailService : MailService,
+    },
+  ],
   exports: [MailService],
 })
 export class MailModule {}
