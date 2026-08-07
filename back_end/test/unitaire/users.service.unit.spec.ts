b5126e0bfde8657820dbd7991dfda100ba6d07e6
LOUIS Romain <romain.louis77@gmail.com>
2026-08-08 01:26:40 +0200

    notification et envoie

--- back_end/test/unitaire/users.service.unit.spec.ts
+++ back_end/test/unitaire/users.service.unit.spec.ts
@@ -17,6 +17,7 @@ import { GamificationProfile } from '../../src/gamification/entities/gamificatio
 import { Article } from '../../src/articles/entities/article.entity';
 import { FishCard } from '../../src/catalog/fish-cards/fish-card.entity';
 import { PlantCard } from '../../src/catalog/plant-cards/plant-card.entity';
+import { Settings } from '../../src/settings/settings.entity';
 
 jest.mock('argon2', () => ({
   hash: jest.fn(async (s: string) => 'hashed:' + s),
@@ -85,6 +86,7 @@ describe('UsersService (unit)', () => {
         { provide: getRepositoryToken(Article), useValue: { find: jest.fn() } },
         { provide: getRepositoryToken(FishCard), useValue: { find: jest.fn() } },
         { provide: getRepositoryToken(PlantCard), useValue: { find: jest.fn() } },
+        { provide: getRepositoryToken(Settings), useValue: { findOne: jest.fn() } },
       ],
     }).compile();
 
